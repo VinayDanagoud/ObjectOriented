5bec0e4b3d1185276305e47d825f75c96cf81354
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-07 13:03:55 +0530

    Object Oriented

---
+++ ObjectOriented/ObjectOriented/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented program");
+            Console.ReadLine();
+        }
+    }
+}
